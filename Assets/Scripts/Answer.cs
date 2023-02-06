f93a0bf32b924a89ab581aa7510f7e2f8002d6d8
Víctor Díaz Fernández <diafervic@IESPIRINGALLA.AD>
2023-02-06 19:39:53 +0100

    Clase Answer corregida. No se había guardado bien

--- Assets/Scripts/Answer.cs
+++ Assets/Scripts/Answer.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Answer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Serializable]
+public class Answer {
+    public  List<Question> results;
 }
