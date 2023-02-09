d6bae7cdf1a7ae2724097983d4e6868f5f0ae920
Víctor Díaz Fernández <diafervic@IESPIRINGALLA.AD>
2023-02-09 22:39:07 +0100

    Ampliación para poder jugar al trivial

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,14 +7,87 @@ public class Main : MonoBehaviour {
 
     public Answer answer;
     private const int numberOfQuestions = 10;
+    private bool isPosibleToGame = false;
+    private int index;
+    private int i,j;
+    private List<string> answers;
+    //private string questionDisplay ="";
+
+    private Question quest;
+    private string correctAnswerDisplay;
+
+    private bool waitingAnswer = false;
+    private int correctAnswer;
     
     // Start is called before the first frame update
     void Start() {
 
         StartCoroutine(GetRequest($"https://opentdb.com/api.php?amount={numberOfQuestions}"));
-        
+        index=-1;
+        Invoke("NextQuestion",0.1f);
+
     }
 
+    void Update() {
+
+        if (! isPosibleToGame || index==numberOfQuestions) {
+            return;
+        }
+
+
+        if (! waitingAnswer) {
+
+            answers = new List<string>();
+
+            quest = answer.results[index];
+            //questionDisplay = quest.question;
+            correctAnswer = Random.Range(0,quest.incorrect_answers.Length);
+            waitingAnswer = true;            
+
+            j=0;
+
+            for (i = 0; i<=quest.incorrect_answers.Length; i++) {
+                if (i == correctAnswer) {
+                    answers.Add(quest.correct_answer);
+                } else {
+                    answers.Add(quest.incorrect_answers[j]);
+                    j++;
+                }
+            }
+
+        } else {
+
+            string userInput = Input.inputString.ToUpper();
+
+            if (userInput != "") {
+                if ((userInput[0] - '0') == correctAnswer) {
+                    correctAnswerDisplay = "<<<<< Respuesta acertada>>>>>: " + answers[correctAnswer];                    
+                } else {
+                    correctAnswerDisplay = "<<<<< Respuesta errónea >>>>> La respuesta correcta es: " + answers[correctAnswer];
+                }
+                Invoke("NextQuestion", 3);
+            }
+        }
+
+    }
+
+    private void NextQuestion() {
+        waitingAnswer = false;
+        index++;
+        correctAnswerDisplay ="";
+    }
+
+    private void OnGUI() {
+        if (isPosibleToGame) {
+            GUI.Label(new Rect(10, 40, 800, 30), quest.question);
+            for (int i=0; i<answers.Count; i++) {
+                GUI.Label(new Rect(30, 70 + i * 30, 800, 30), i + " -> " + answers[i]);
+            }
+
+            GUI.Label(new Rect(10, 200, 800, 30), "Respuesta ... ");
+            GUI.Label(new Rect (10, 250, 800, 30), correctAnswerDisplay);
+        }
+    }
 
     IEnumerator GetRequest(string uri) {
 
@@ -45,19 +118,11 @@ public class Main : MonoBehaviour {
         
         answer = JsonUtility.FromJson<Answer>(jsonText);
         if (answer.results.Count > 0) {
-            PrintResults();
+            Debug.Log("<<<<<<<<<<<<<<<<<<<<<<< " + answer.results.Count);
+            isPosibleToGame = true;
         } else {
             Debug.Log("No se ha recibido ninguna pregunta.");
         }
-    }
-
-    private void PrintResults() {
-        Question question = answer.results[0];
-        Debug.Log($"Nº de preguntas solicitadas: {numberOfQuestions}");
-        Debug.Log($"Categoría: {question.category}.");
-        Debug.Log($"Dificultad: {question.difficulty}.");
-        Debug.Log($"Pregunta: {question.question}.");
-        Debug.Log($"Respuesta correcta: {question.correct_answer}");
     }
 
 }
